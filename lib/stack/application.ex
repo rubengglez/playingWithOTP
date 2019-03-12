368abb47f79f4d4ebcbe0e4b11c59a1c795ccc2d
Rubén <ruben.garcia@quobis.com>
2019-03-12 21:26:53 +0100

    add a supervisor

---
+++ lib/stack/application.ex
@@ -0,0 +1,25 @@
+defmodule Stack.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: StackSup.Worker.start_link(arg)
+      # {StackSup.Worker, arg},
+      # Stack.start_link([])
+      %{
+        id: Stack,
+        start: {Stack, :start_link, [[2]]}
+      }
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Stack.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
